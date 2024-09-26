df63c71ee36c28ca013ca254fab231280e17e018
Karuna Kumari <mishrakaruna9142@gmail.com>
2024-09-26 22:36:14 +0530

    Add files via upload

---
+++ HashSetEx1.java
@@ -0,0 +1,24 @@
+package com.setInterface;
+import java.util.Collections;
+import java.util.HashSet;
+
+public class HashSetEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		HashSet<Integer> h1 = new HashSet<>();
+		
+		
+		h1.add(23);
+		h1.add(45);
+		
+		System.out.println(h1);
+		
+		h1.remove(23);
+		h1.contains(1);
+		
+		
+	}
+
+}
