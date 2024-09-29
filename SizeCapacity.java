0caec4948a5f9c1d9c1a1997ba11d5ab65138b16
Karuna Kumari <mishrakaruna9142@gmail.com>
2024-09-29 23:52:12 +0530

    Add files via upload

---
+++ SizeCapacity.java
@@ -0,0 +1,15 @@
+package com.arrayList;
+
+import java.util.ArrayList;
+
+public class SizeCapacity {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> a1 = new ArrayList<>(10);
+		
+		System.out.println("Capacity of Array List:- "+a1.size());
+
+	}
+
+}
